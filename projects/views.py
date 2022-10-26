e3b139d6cfd56788b9a01f12680f3cd3f4dde739
omarjunior <omarhernandez041204@gmail.com>
2022-10-26 10:47:20 -0500

    first comitt

---
+++ projects/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http.response import HttpResponse
+
+# Create your views here.
+def projects(request):
+    return render(request, "projects/projects.html", {})
+
+def project(request, pk):
+    return render(request, "projects/single-project.html", {'pk': pk})
