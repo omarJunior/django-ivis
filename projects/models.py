226c9545082a885e3c60db8ce5bf086f68bbb736
Omar Junior <omarhernandez041204@gmail.com>
2022-10-28 16:08:59 -0500

    return str

--- projects/models.py
+++ projects/models.py
@@ -10,3 +10,6 @@ class Project(models.Model):
     created = models.DateTimeField(auto_now_add = True)
     updated = models.DateTimeField(auto_now = True)
     id = models.UUIDField(default = uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.title)
