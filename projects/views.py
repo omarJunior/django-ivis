47d29337c04d42a2c247353420fc11ba380d29fc
Omar Junior <omarhernandez041204@gmail.com>
2022-10-28 14:59:55 -0500

    cambios

--- projects/views.py
+++ projects/views.py
@@ -3,17 +3,17 @@ from django.shortcuts import render
 # Create your views here.
 projectList = [
     {
-        "id": 1,
+        "id": "1",
         "nombre": "valleapp",
         "descripcion": "Valle app PRO",
     },
     {
-        "id": 2,
+        "id": "2",
         "nombre": "direktu",
         "descripcion": "Direktu app PRO",
     },
     {
-        "id": 3,
+        "id": "3",
         "nombre": "Gestion Requerimientos",
         "descripcion": "Gestion Requerimientos PRO",
     }
@@ -21,13 +21,13 @@ projectList = [
 
 def projects(request):
     msg = "All projects"
-    number = 10
+    number = 20
     context = {'msg': msg, 'number': number, 'projects': projectList}
     return render(request, "projects/projects.html", context)
 
 def project(request, pk):
     projectObj = None
     for i in projectList:
-        if str(i['id']) == pk:
+        if i['id'] == pk:
             projectObj = i
     return render(request, "projects/single-project.html", projectObj)
