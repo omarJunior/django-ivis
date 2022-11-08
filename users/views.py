92bda09c8e143e11ae47e0b18b3b7905fe9f76ee
Omar Junior <omarhernandez041204@gmail.com>
2022-11-08 15:41:01 -0500

    cambios actualizados

--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
+from .forms import CustomUserCreationForm
 from django.contrib.auth.models import User
 from users.models import *
 
@@ -39,14 +40,18 @@ def logoutUser(request):
 
 def registerUser(request):
     page = 'register'
-    form = UserCreationForm()
+    form = CustomUserCreationForm()
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False) #create a instance
             user.username = user.username.lower()
             user.save()
             messages.success(request, "User account was created")
+            login(request, user)
+            return redirect('profile')
+        else:
+            messages.error(request, 'An error has occured during registration')
 
     context = {'page': page, 'form': form}
     return render(request, "users/login_register.html", context)
