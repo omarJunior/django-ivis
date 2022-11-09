5e9160050716ff212a86bffb0fa809b76258232a
Omar Junior <omarhernandez041204@gmail.com>
2022-11-09 14:51:27 -0500

    cambios actualizados

--- users/urls.py
+++ users/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('logout/', views.logoutUser, name="logout"),
     path('register/', views.registerUser, name="register"),
     path('profile/<str:pk>/', views.userProfile, name="user_profile"),
+    path('account/', views.userAccount, name="account"),
 ]
--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 from .forms import CustomUserCreationForm
 from django.contrib.auth.models import User
 from users.models import *
@@ -70,4 +71,12 @@ def userProfile(request, pk):
     
     context = {'profile': profile, 'topSkills': topSkills, 'otherSkills': otherSkills}
     return render(request, "users/user-profile.html", context)
+
+@login_required
+def userAccount(request):
+    if request.user.is_authenticated:
+        profile = request.user.profile
+        context = {'profile': profile}
+        return render(request, "users/account.html", context)
+    return redirect('login')
 
